c37f9a1d10adcefc285804d370840e649b8aa259
Priyanshu085 <abpriyanshu007@gmail.com>
2023-10-24 09:50:22 +0530

    ADD Openai input button

--- main.py
+++ main.py
@@ -5,45 +5,58 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from langchain.prompts import ChatPromptTemplate
 from dotenv import load_dotenv, find_dotenv
-
-load_dotenv(find_dotenv())
-openai.api_key = os.environ['OPENAI_API_KEY']
-
-llm = OpenAI()
-chat_model = ChatOpenAI()
-
 import streamlit as st
 
-# Input the Required Values
-name = st.text_input("Enter The Project Name")
-tech = st.multiselect("Enter the Tech Stack used in the Project", ['ReactJS','NextJS','MongoDB','NodeJS','JavaScript','TypeScript'])
-desc = st.text_input("Write a breif about your project")
+load_dotenv(find_dotenv())
 
-if st.button('Generate README file'):
-  if ( name and tech and desc ):
-    my_template = '''
-    Project name {name}.
-    Tech Stack is/are {tech}.
-    A Breif desription about my project is {desc}.
 
-    \'\'\'Include the columns in following order in the README File.\'\'\'
-    - Table of Content
-    - Screenshot of project
-    - Description (in 50 words)
-    - Features (in 5 Bullet Points)
-    - Technology Used
-    - Usage
-    - Deployment
-    - License 
+st.set_page_config(
+    page_title="Smart Readme",
+    # page_icon="",
+)
 
-    \* Make the Deployment and License part as optional for the user
+# Create an empty slot for the radio button
+element_slot = st.empty()
 
-    Create a README file in markdown. Also fill the details appropriately.
+openai.api_key = element_slot.text_input("Enter the Open AI API KEY")
 
-    '''
+if(openai.api_key):
 
-    prompt_template = ChatPromptTemplate.from_template(template=my_template)
+  llm = OpenAI()
+  chat_model = ChatOpenAI()
 
-    chain = LLMChain(llm=llm, prompt=prompt_template)
+  element_slot.empty()
 
-    st.markdown(chain.predict(name=name, desc=desc, tech=tech))
+  # Input the Required Values
+  name = st.text_input("Enter The Project Name")
+  tech = st.multiselect("Enter the Tech Stack used in the Project", ['ReactJS','NextJS','MongoDB','NodeJS','JavaScript','TypeScript'])
+  desc = st.text_input("Write a breif about your project")
+
+  if st.button('Generate README file'):
+    if ( name and tech and desc ):
+      my_template = '''
+      Project name {name}.
+      Tech Stack is/are {tech}.
+      A Breif desription about my project is {desc}.
+
+      \'\'\'Include the columns in following order in the README File.\'\'\'
+      - Table of Content
+      - Screenshot of project
+      - Description (in 50 words)
+      - Features (in 5 Bullet Points)
+      - Technology Used
+      - Usage
+      - Deployment
+      - License 
+
+      \* Make the Deployment and License part as optional for the user
+
+      Create a README file in markdown. Also fill the details appropriately.
+
+      '''
+
+      prompt_template = ChatPromptTemplate.from_template(template=my_template)
+
+      chain = LLMChain(llm=llm, prompt=prompt_template)
+
+      st.markdown(chain.predict(name=name, desc=desc, tech=tech))
